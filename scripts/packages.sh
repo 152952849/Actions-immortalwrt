ee48ad8baae45849e017b6b369a9c66156764ed4
152952849 <152952849@qq.com>
2026-04-18 11:35:06 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -23,7 +23,8 @@ git clone -b master --depth 1 https://github.com/eamonxg/luci-theme-aurora.git
 git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
 
 # adguardhome-nft
-git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+git clone https://github.com/kenzok78/luci-app-adguardhome.git
+#git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #git clone https://github.com/w9315273/luci-app-adguardhome.git
 
 # lucky
